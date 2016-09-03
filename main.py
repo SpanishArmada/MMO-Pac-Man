c49c31c7ea6750a2c7cf65568453892ab6eb66bd
Dian Bakti <dbakti1605@gmail.com>
2016-09-03 19:55:49 +0800

    refactoring

--- main.py
+++ main.py
@@ -24,54 +24,57 @@ class SocketHandler(tornado.websocket.WebSocketHandler):
             data.append(self)
             counter += 1
             players.append(Player(GE, counter, 1, 1))
-            msg = {"type": 0, "playerID": counter}
+            msg = {"type": 0, "player_id": counter}
             self.write_message(msg)
 
             
     def on_message(self, msg):
-        incomingData = json.loads(msg)
-        msgType = incomingData["type"]
-        print(msgType)
-        if(msgType == 0):
-            row = incomingData["row"]
-            col = incomingData["col"]
-            playerID = incomingData["playerID"]
+        incoming_data = json.loads(msg)
+        msg_type = incoming_data["type"]
+        print(msg_type)
+        if(msg_type == 0):
+            row = incoming_data["row"]
+            col = incoming_data["col"]
+            player_id = incoming_data["player_id"]
             print("row", row)
-            localGrids = GE.get_arena().grids[row - 9:row + 10]
-            print("len", len(localGrids))
+            local_grids = GE.get_arena().grids[row - 9:row + 10]
+            print("len", len(local_grids))
             grids = []
-            for i in localGrids:
-                currentRow = i[col - 16:col + 17]
+            food_pos = []
+            for i in local_grids:
+                current_row = i[col - 16:col + 17]
                 r = []
-                for j in currentRow:
+                for j in current_row:
                     if(j.get_typ == 4):
                         r.append(1)
                     else:
                         r.append(0)
+                    if(j.get_typ == 1):
+                        food_pospend([j.get_x(), j.get_y()])
                 grids.append(r)
             print(grids)
-            pacPos = dict()
-            ghostPos = dict()
+            pac_pos = dict()
+            ghost_pos = dict()
             for i in players:
-                pacPos[str(i.get_id())] = (i.get_x(), i.get_y())
+                pac_pos[str(i.get_id())] = (i.get_x(), i.get_y())
             for i in ghosts:
-                ghostPos[str(i.get_id())] = (i.get_x(), i.get_y())
+                ghost_pos[str(i.get_id())] = (i.get_x(), i.get_y())
             print(len(players))
             p = None
             for x in players:
-                if x.get_id() == playerID:
+                if x.get_id() == player_id:
                     p = x
                     break
-            data = {"type": 1, "grids": grids, "pacPos": pacPos, "ghostPos": ghostPos, "score": p.get_score()}
+            data = {"type": 1, "grids": grids, "pac_pos": pac_pos, "ghost_pos": ghost_pos, "food_pos": food_pos, "score": p.get_score()}
             self.write_message(data)
         else:
             # closed
             print("in")
-            playerID = incomingData["playerID"]
-            print("delete", playerID)
+            player_id = incoming_data["player_id"]
+            print("delete", player_id)
             p = None
             for x in players:
-                if x.get_id() == playerID:
+                if x.get_id() == player_id:
                     p = x
                     break
             players.remove(p)
